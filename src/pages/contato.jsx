27a91c01560afd85df0723999827063e7912570c
Vitor <128196980+VitorGGama@users.noreply.github.com>
2024-01-17 11:15:22 -0300

    ajuste para uso da tag main

---
+++ src/pages/contato.jsx
@@ -0,0 +1,9 @@
+export default function Contato() {
+  return (
+    <>
+      <section>
+        <h2>Fale conosco</h2>
+      </section>
+    </>
+  );
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <>
-      <h1>PetShop</h1>
+      <section>
+        <h2>Pet Noticias</h2>
+      </section>
     </>
   );
 }
