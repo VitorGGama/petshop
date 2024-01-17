91792af2421f5ab27b190701b62e791617c2fe41
Vitor <128196980+VitorGGama@users.noreply.github.com>
2024-01-17 09:57:06 -0300

    favicon metatag authot pt br

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,21 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="pt-br">
+      <Head>
+        <link
+          rel="shortcurt icon"
+          href="/images/favicon.png"
+          type="image/png"
+          sizes="256x256"
+        />
+        <meta name="author" content="Vitor G. Gama" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
