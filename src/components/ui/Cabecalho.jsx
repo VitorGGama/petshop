11b5fd8994939e21b5d60dc0971200079a96e6c9
Vitor <128196980+VitorGGama@users.noreply.github.com>
2024-01-18 09:08:35 -0300

    configuracao do cabecalho

--- src/components/ui/Cabecalho.jsx
+++ src/components/ui/Cabecalho.jsx
@@ -1,20 +1,38 @@
 import Link from "next/link";
 import Image from "next/image";
+import styled from "styled-components"; // Adiciona esta linha para importar o styled-components
 import Menu from "./Menu";
 
 export default function Cabecalho() {
   return (
-    <header>
+    <StyledHeader>
       <h1>
-        <Image
-          src="/images/logo.png"
-          width={48}
-          height={48}
-          alt="Patinha dentro de um coração"
-        />
-        <Link href="/">Petshop</Link>
+        <Link href="/">
+          <Image
+            src="/images/logo.png"
+            width={48}
+            height={48}
+            alt="Patinha dentro de um coração"
+          />
+          Petshop
+        </Link>
       </h1>
       <Menu />
-    </header>
+    </StyledHeader>
   );
 }
+const StyledHeader = styled.header`
+  background-color: white;
+  box-shadow: var(--sombra-box);
+
+  h1 a {
+    text-decoration: none;
+    color: var(--cor-primaria);
+    background-color: var(--cor-primaria-fundo);
+    padding: 0.5rem 1.2rem;
+    font-size: 1.5rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
