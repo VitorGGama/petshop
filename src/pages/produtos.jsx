6a7f12e160729a9af66e8d9957517f467170c64b
Vitor <128196980+VitorGGama@users.noreply.github.com>
2024-01-19 08:28:52 -0300

    conteudo da pagina produtos

--- src/pages/produtos.jsx
+++ src/pages/produtos.jsx
@@ -10,6 +10,34 @@ export default function Produtos() {
       </Head>
       <StyledProdutos>
         <h2>Conheça nossos produtos</h2>
+
+        <article>
+          <h3>Tosa</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Necessitatibus debitis beatae, quo eius harum provident optio
+            accusamus nulla, possimus commodi expedita asperiores! Modi
+            repellendus soluta, dignissimos quo maxime omnis nihil?
+          </p>
+        </article>
+        <article>
+          <h3>Banho</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Necessitatibus debitis beatae, quo eius harum provident optio
+            accusamus nulla, possimus commodi expedita asperiores! Modi
+            repellendus soluta, dignissimos quo maxime omnis nihil?
+          </p>
+        </article>
+        <article>
+          <h3>Castração</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Necessitatibus debitis beatae, quo eius harum provident optio
+            accusamus nulla, possimus commodi expedita asperiores! Modi
+            repellendus soluta, dignissimos quo maxime omnis nihil?
+          </p>
+        </article>
       </StyledProdutos>
     </>
   );
