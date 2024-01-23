26c5ef6d5fbf021a689b5b48e834321c9ce500cc
Vitor <128196980+VitorGGama@users.noreply.github.com>
2024-01-23 11:24:16 -0300

    getStaticPaths

--- src/pages/posts/[id].jsx
+++ src/pages/posts/[id].jsx
@@ -1,3 +1,39 @@
-export default function Post() {
-  return <h2>Página Post</h2>;
+import Container from "@/components/ui/Container";
+import Head from "next/head";
+import styled from "styled-components";
+
+export async function getStaticProps({ params }) {
+  const { id } = params;
+  console.log(id);
 }
+
+export async function getStaticPaths() {
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
+}
+
+export default function Post() {
+  return (
+    <>
+      <Head>
+        <title>Titulo do Post... - Pet Shop</title>
+        <meta name="description" content="Descrição do post" />
+      </Head>
+      <StyledPost>
+        <h2>Título do post....</h2>
+        <Container>
+          <h3>Categoria do post...</h3>
+          <p>Descrição do post...</p>
+        </Container>
+      </StyledPost>
+    </>
+  );
+}
+
+const StyledPost = styled.article`
+  h2::before {
+    content: "📑 ";
+  }
+`;
