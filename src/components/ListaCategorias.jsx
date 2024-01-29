bd5b98233adf6d44cd1845c9cea241c4f0d95bad
Vitor <128196980+VitorGGama@users.noreply.github.com>
2024-01-29 10:31:04 -0300

    componets ListaCategorias

---
+++ src/components/ListaCategorias.jsx
@@ -0,0 +1,62 @@
+// ListaCategorias.jsx
+import styled from "styled-components";
+
+const ListaCategorias = ({
+  categorias,
+  filtrar,
+  limparFiltro,
+  filtroAtivo,
+}) => {
+  return (
+    <StyledCategorias>
+      {categorias.map((categoria, indice) => (
+        <button onClick={filtrar} key={indice}>
+          {categoria}
+        </button>
+      ))}
+
+      {filtroAtivo && (
+        <button className="limpar" onClick={limparFiltro}>
+          Limpar filtro
+        </button>
+      )}
+    </StyledCategorias>
+  );
+};
+
+const StyledCategorias = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  margin: 1rem 0;
+  flex-wrap: wrap;
+
+  button {
+    text-transform: capitalize;
+    border: none;
+    background-color: var(--cor-secundaria-fundo);
+    color: #f7f7f7;
+    padding: 0.5rem;
+    border-radius: var(--borda-arredondada);
+
+    &:hover,
+    &:focus {
+      background-color: var(--cor-secundaria-fundo-hover);
+      cursor: pointer;
+    }
+  }
+
+  .limpar {
+    background-color: gray;
+
+    &:hover {
+      background-color: slategray;
+    }
+
+    &::before {
+      content: "🧹 ";
+    }
+  }
+`;
+
+export default ListaCategorias;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,18 +1,18 @@
 import Head from "next/head";
 import styled from "styled-components";
 import ListaPosts from "@/components/ListaPosts";
+import ListaCategorias from "@/components/ListaCategorias";
 import { useState } from "react";
 import serverApi from "./api/server";
 
 export async function getStaticProps() {
   try {
     const resposta = await fetch(`${serverApi}/posts`);
-    const dados = await resposta.json();
-
     if (!resposta.ok) {
       throw new Error(`Erro: ${resposta.status} - ${resposta.statusText}`);
     }
 
+    const dados = await resposta.json();
     const categorias = dados.map((post) => post.categoria);
     const categoriasUnicas = [...new Set(categorias)];
 
@@ -23,7 +23,7 @@ export async function getStaticProps() {
       },
     };
   } catch (error) {
-    console.error("Deu ruim: " + error.message);
+    console.error("Erro ao obter os dados: " + error.message);
     return {
       notFound: true,
     };
@@ -37,7 +37,6 @@ export default function Home({ posts, categorias }) {
 
   const filtrar = (event) => {
     const categoriaEscolhida = event.currentTarget.textContent;
-
     const novaListaDePosts = posts.filter(
       (post) => post.categoria === categoriaEscolhida
     );
@@ -70,67 +69,18 @@ export default function Home({ posts, categorias }) {
       <StyledHome>
         <h2>Pet Notícias</h2>
 
-        <StyledCategorias>
-          {categorias.map((categoria, indice) => {
-            return (
-              <button
-                className={categoria === categoriaAtiva ? "ativo" : ""}
-                onClick={filtrar}
-                key={indice}
-              >
-                {categoria}
-              </button>
-            );
-          })}
-
-          {filtroAtivo && (
-            <button onClick={limparFiltro} className="limpar">
-              Limpar filtro
-            </button>
-          )}
-        </StyledCategorias>
+        <ListaCategorias
+          categorias={categorias}
+          filtrar={filtrar}
+          limparFiltro={limparFiltro}
+          filtroAtivo={filtroAtivo}
+        />
 
         <ListaPosts posts={listaDePosts} />
       </StyledHome>
     </>
   );
 }
-
-const StyledCategorias = styled.div`
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  margin: 1rem 0;
-  flex-wrap: wrap;
-
-  button {
-    text-transform: capitalize;
-    border: none;
-    background-color: var(--cor-secundaria-fundo);
-    color: #f7f7f7;
-    padding: 0.5rem;
-    border-radius: var(--borda-arredondada);
-
-    &:hover,
-    &:focus {
-      background-color: var(--cor-secundaria-fundo-hover);
-      cursor: pointer;
-    }
-    &.ativo {
-      background-color: var(--cor-primaria-fundo);
-    }
-  }
-
-  .limpar {
-    background-color: gray;
-    &:hover {
-      background-color: slategray;
-    }
-    &::before {
-      content: "🧹 ";
-    }
-  }
-`;
 
 const StyledHome = styled.section`
   h2::before {
