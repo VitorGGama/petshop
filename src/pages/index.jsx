c238168d668a8f71e15603dabeb69696e8ffbfa1
Vitor <128196980+VitorGGama@users.noreply.github.com>
2024-01-30 11:16:53 -0300

    Substituição dos dados por arrayDePosts

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,50 +1,45 @@
 import Head from "next/head";
+import { useState } from "react";
 import styled from "styled-components";
 import ListaPosts from "@/components/ListaPosts";
 import ListaCategorias from "@/components/ListaCategorias";
-import { useState } from "react";
 import serverApi from "./api/server";
 
 export async function getStaticProps() {
   try {
     const resposta = await fetch(`${serverApi}/posts.json`);
     const dados = await resposta.json();
+
     if (!resposta.ok) {
       throw new Error(`Erro: ${resposta.status} - ${resposta.statusText}`);
     }
 
-    /* Colocando os dados dos objetos dentro de uma array 
+    /* Colocando os dados dos objetos dentro de um array 
     
-    1) Object.keys(dados): extrair as chaves/id de cada objeyto para um array.
-    
+    1) Object.keys(dados): extrair as chaves/id de cada objeto para um array.
+
     2) Map no array de chaves, em que retornamos um novo objeto.
-    
-    3) Cada novo objeto (representado por post) é criado com os dados
-    existentes (por isso, usamos o spread)
-    
-    4) No caso do id, atribuimos a própria chave de cada objeto. Portanto,
-    em vez de ids numéricos, os ids passam a ser na aplicação o próprio hash/código
-    de cada post. */
 
-    const arrayDePosts = Object.keys(dados).map((post) => {
-      return {
-        ...dados,
-        id: post,
-      };
-    });
-    console.log(arrayDePosts);
+    3) Cada novo objeto (representado por post) é criado com
+    os dados existentes (por isso, usamos o spread)
 
-    const categorias = dados.map((post) => post.categoria);
+    4) No caso do id, atribuímos a própria chave de cada objeto. Portanto, em vez de ids numéricos, os ids passam a ser na aplicação o próprio hash/código de cada post.  */
+    const arrayDePosts = Object.keys(dados).map((post) => ({
+      ...dados[post],
+      id: post,
+    }));
+
+    const categorias = arrayDePosts.map((post) => post.categoria);
     const categoriasUnicas = [...new Set(categorias)];
 
     return {
       props: {
-        posts: dados,
+        posts: arrayDePosts,
         categorias: categoriasUnicas,
       },
     };
   } catch (error) {
-    console.error("Erro ao obter os dados: " + error.message);
+    console.error("Deu ruim: " + error.message);
     return {
       notFound: true,
     };
@@ -54,10 +49,11 @@ export async function getStaticProps() {
 export default function Home({ posts, categorias }) {
   const [listaDePosts, setListaDePosts] = useState(posts);
   const [filtroAtivo, setFiltroAtivo] = useState(false);
-  const [categoriaAtiva, setCategoriaAtivo] = useState("");
+  const [categoriaAtiva, setCategoriaAtiva] = useState("");
 
   const filtrar = (event) => {
     const categoriaEscolhida = event.currentTarget.textContent;
+
     const novaListaDePosts = posts.filter(
       (post) => post.categoria === categoriaEscolhida
     );
@@ -65,7 +61,9 @@ export default function Home({ posts, categorias }) {
     // Sinalizando o state como filtro ativo (true)
     setFiltroAtivo(true);
     setListaDePosts(novaListaDePosts);
-    setCategoriaAtivo(categoriaEscolhida);
+
+    // Sinalizando o state com o texto/categoria escolhida
+    setCategoriaAtiva(categoriaEscolhida);
   };
 
   const limparFiltro = () => {
@@ -74,7 +72,9 @@ export default function Home({ posts, categorias }) {
 
     // Atualizando o state da listaDePosts para os posts originais
     setListaDePosts(posts);
-    setCategoriaAtivo("");
+
+    // Atualizando o state da categoria ativa para vazio ""
+    setCategoriaAtiva("");
   };
 
   return (
@@ -92,12 +92,11 @@ export default function Home({ posts, categorias }) {
 
         <ListaCategorias
           categorias={categorias}
-          filtrar={filtrar}
-          limparFiltro={limparFiltro}
-          filtroAtivo={filtroAtivo}
           categoriaAtiva={categoriaAtiva}
+          onFiltrar={filtrar}
+          onLimparFiltro={limparFiltro}
+          filtroAtivo={filtroAtivo}
         />
-
         <ListaPosts posts={listaDePosts} />
       </StyledHome>
     </>
--- src/pages/posts/[id].jsx
+++ src/pages/posts/[id].jsx
@@ -4,15 +4,10 @@ import styled from "styled-components";
 import serverApi from "../api/server";
 
 export async function getStaticProps({ params }) {
-  /* Utilizamos a prop params do getStaticProps
-  para poder ter acesso aos parâmetros dinâmicos da rota
-  configurada nos links da lista de Posts. Usamos a 
-  desestruturação para obter de forma direta o parâmetro 
-  chamado "id". */
   const { id } = params;
 
   try {
-    const resposta = await fetch(`${serverApi}/posts/${id}`);
+    const resposta = await fetch(`${serverApi}/posts/${id}.json`);
 
     if (!resposta.ok) {
       throw new Error(`Erro: ${resposta.status} - ${resposta.statusText}`);
@@ -32,19 +27,10 @@ export async function getStaticProps({ params }) {
     };
   }
 }
-/* getStaticPaths é obrigatória quando se trata
-de trabalhar com páginas/rotas dinâmicas, ou seja, que
-dependem de parâmetros para serem construídas. */
+
 export async function getStaticPaths() {
   return {
-    /* Paths fica vazio pois todos os caminhos devem ser 
-    gerados sob demanda, ou seja, no momento em que a 
-    página for aberta.*/
     paths: [],
-
-    /* fallback fica como "blocking" para garantir que
-    a página somente será renderizada após a conclusão
-    da geração dos caminhos e dos dados estáticos. */
     fallback: "blocking",
   };
 }
